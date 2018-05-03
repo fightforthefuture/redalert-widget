480cebc92928f5f95b57c1c9fefd47e5e5fb499f
Ken Mickles <ken@37i.net>
2018-05-02 22:17:56 -0400

    Clean up widget.js

--- static/widget.js
+++ static/widget.js
@@ -1,186 +1,175 @@
 (function() {
   'use strict';
 
+  var domId = 'RED_ALERT_WIDGET';
+  var animationDuration = 200;
+
+  // user-configurable options
   var opts = window.RED_ALERT_OPTIONS || {};
-  var resetStyles = {};
+  var org = opts.org || null;
+  var cookieExpirationDays = parseFloat(opts.cookieExpirationDays || 1);
+  var alwaysShow = !!(opts.alwaysShow || window.location.hash.indexOf('ALWAYS_SHOW_RED_ALERT') !== -1);
+  var disableGoogleAnalytics = !!opts.disableGoogleAnalytics;
+  var iframeHost = opts.iframeHost !== undefined ? opts.iframeHost : 'https://redalert.battleforthenet.com';
+  var position = opts.position || null;
+  var cowardlyRefuseToMaximize = !!opts.cowardlyRefuseToMaximize;
 
-  var widget = {
-    id: 'RED_ALERT_WIDGET',
-    org: (opts.org || null),
-    cookieExpirationDays: parseFloat((opts.cookieExpirationDays || 1)),
-    alwaysShow: !!opts.alwaysShow,
-    disableGoogleAnalytics: !!opts.disableGoogleAnalytics,
-    iframeHost: (typeof(opts.iframeHost) === 'undefined' ? 'https://redalert.battleforthenet.com' : opts.iframeHost),
-    position: (opts.position || null),
-    cowardlyRefuseToMaximize: !!opts.cowardlyRefuseToMaximize,
-    animationDuration: 200,
+  var stylesToReset = {};
 
-    maximize: function() {
-      resetStyles = {
-        overflow: document.body.style.overflow
-      };
+  function maximize() {
+    stylesToReset = {
+      overflow: document.body.style.overflow
+    };
 
-      document.getElementById(this.id).classList.add('RAW--maximized');
+    document.getElementById(domId).classList.add('RAW--maximized');
 
-      setTimeout(function(){
-        document.body.style.overflow = 'hidden';
+    setTimeout(function(){
+      document.body.style.overflow = 'hidden';
 
-        if (window.innerWidth < 500) {
-          resetStyles.position = document.body.style.position;
-          resetStyles.scrollTop = window.pageYOffset;
-          document.body.style.position = 'fixed';
-        }
-      }, this.animationDuration);
-    },
+      var isProbablyMobile = window.innerWidth < 500;
 
-    closeWindow: function() {
-      document.body.style.overflow = resetStyles.overflow;
-
-      if (resetStyles.position !== undefined) {
-        document.body.style.position = resetStyles.position;
+      if (isProbablyMobile) {
+        stylesToReset.position = document.body.style.position;
+        stylesToReset.scrollTop = window.pageYOffset;
+        document.body.style.position = 'fixed';
       }
-
-      if (resetStyles.scrollTop !== undefined) {
-        window.scrollTo(0, resetStyles.scrollTop);
-      }
-
-      window.removeEventListener('message', onMessageReceived);
-
-      var el = document.getElementById(this.id);
-      el.classList.add('RAW--closing')
-      setTimeout(function(){
-        el.parentNode.removeChild(el);
-      }, this.animationDuration);
-    },
-
-    getCookie: function(cname) {
-      var name = cname + "=";
-      var ca = document.cookie.split(';');
-      var c;
-
-      for(var i = 0; i < ca.length; i++) {
-        c = ca[i].trim();
-        if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-        }
-      }
-
-      return "";
-    },
-
-    setCookie: function(name, val, exdays) {
-      var d = new Date();
-      d.setTime(d.getTime()+(exdays*24*60*60*1000));
-
-      var expires = "expires="+d.toGMTString();
-      document.cookie = name + "=" + val + "; " + expires + "; path=/";
-    },
-
-    getIframeSrc: function() {
-      var src = this.iframeHost + '/?';
-
-      if (this.org) {
-        src += 'org=' + encodeURIComponent(this.org) + '&';
-      }
-
-      if (this.disableGoogleAnalytics) {
-        src += 'ga=false&';
-      }
-
-      if (this.position) {
-        src += 'position=' + this.position + '&';
-      }
-
-      if (this.cowardlyRefuseToMaximize) {
-        src += 'dayofaction=false&';
-      }
-
-      return src.replace(/(\?|&)$/, '');
-    },
-
-    createIframe: function() {
-      var wrapper = document.createElement('div');
-      wrapper.id = this.id;
-      var iframe = document.createElement('iframe');
-      iframe.src = this.getIframeSrc();
-      iframe.frameBorder = 0;
-      iframe.allowTransparency = true;
-      // iframe.style.display = 'none';
-      wrapper.appendChild(iframe);
-      document.body.appendChild(wrapper);
-      return wrapper;
-    },
-
-    injectCSS: function(id, css) {
-      var style = document.createElement('style');
-      style.type = 'text/css';
-      style.id = id;
-      if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-      }
-      else {
-        style.appendChild(document.createTextNode(css));
-      }
-      document.head.appendChild(style);
-    },
-
-    receiveMessage: function(event) {
-      if (!event.data.RED_ALERT) return;
-
-      switch (event.data.action) {
-        case 'maximize':
-          return this.maximize();
-        case 'closeWindow':
-          return this.closeWindow();
-      }
-    },
-
-    init: function() {
-      var cookieName = '_RED_ALERT_WIDGET_SHOWN';
-
-      if (this.alwaysShow || window.location.href.indexOf(this.id) !== -1 || !this.getCookie(cookieName)) {
-        this.setCookie(cookieName, 'true', this.cookieExpirationDays);
-
-        var left, right;
-
-        if (this.position === 'left') {
-          left = '0';
-          right = 'auto';
-        }
-        else {
-          left = 'auto';
-          right = '0';
-        }
-
-        this.injectCSS('RED_ALERT_CSS',
-          '#' + this.id + ' { position: fixed; right: ' + right + '; left: ' + left + '; bottom: 0px; width: 450px; height: 350px; z-index: 20000; -webkit-overflow-scrolling: touch; overflow: hidden; transition: width ' + this.animationDuration + 'ms ease-in, height ' + this.animationDuration + 'ms ease-in; } ' +
-          '#' + this.id + '.RAW--maximized { width: 100%; height: 100%; } ' +
-          '#' + this.id + '.RAW--closing { transform: scale(0); transform-origin: bottom right; opacity: 0; transition: transform ' + this.animationDuration + 'ms ease-in, opacity ' + this.animationDuration + 'ms ease-in; } ' +
-          '#' + this.id + ' iframe { width: 100%; height: 100%; }'
-        );
-
-        this.createIframe();
-
-        window.addEventListener('message', onMessageReceived);
-      }
-
-      document.removeEventListener('DOMContentLoaded', onDOMContentLoaded);
-    }
-  };
-
-  function onDOMContentLoaded() {
-    widget.init();
+    }, animationDuration);
   }
 
-  function onMessageReceived(event) {
+  function closeWindow() {
+    document.body.style.overflow = stylesToReset.overflow;
+
+    if (stylesToReset.position !== undefined) {
+      document.body.style.position = stylesToReset.position;
+    }
+
+    if (stylesToReset.scrollTop !== undefined) {
+      window.scrollTo(0, stylesToReset.scrollTop);
+    }
+
+    window.removeEventListener('message', receiveMessage);
+
+    var el = document.getElementById(domId);
+    el.classList.add('RAW--closing')
+    setTimeout(function(){
+      el.parentNode.removeChild(el);
+    }, animationDuration);
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    var c;
+
+    for(var i = 0; i < ca.length; i++) {
+      c = ca[i].trim();
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+
+    return "";
+  }
+
+  function setCookie(name, val, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime()+(exdays*24*60*60*1000));
+
+    var expires = "expires="+d.toGMTString();
+    document.cookie = name + "=" + val + "; " + expires + "; path=/";
+  }
+
+  function getIframeSrc() {
+    var src = iframeHost + '/?';
+
+    if (org) {
+      src += 'org=' + encodeURIComponent(org) + '&';
+    }
+
+    if (disableGoogleAnalytics) {
+      src += 'ga=false&';
+    }
+
+    if (position) {
+      src += 'position=' + position + '&';
+    }
+
+    if (cowardlyRefuseToMaximize) {
+      src += 'dayofaction=false&';
+    }
+
+    return src.replace(/(\?|&)$/, '');
+  }
+
+  function createIframe() {
+    var wrapper = document.createElement('div');
+    wrapper.id = domId;
+    var iframe = document.createElement('iframe');
+    iframe.src = getIframeSrc();
+    iframe.frameBorder = 0;
+    iframe.allowTransparency = true;
+    // iframe.style.display = 'none';
+    wrapper.appendChild(iframe);
+    document.body.appendChild(wrapper);
+    return wrapper;
+  }
+
+  function injectCSS(id, css) {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.id = id;
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    }
+    else {
+      style.appendChild(document.createTextNode(css));
+    }
+    document.head.appendChild(style);
+  }
+
+  function receiveMessage(event) {
     if (!event.data.RED_ALERT) return;
 
     switch (event.data.action) {
       case 'maximize':
-        return widget.maximize();
+        return maximize();
       case 'closeWindow':
-        return widget.closeWindow();
+        return closeWindow();
     }
+  }
+
+  function init() {
+    var cookieName = '_RED_ALERT_WIDGET_SHOWN';
+    var hasNotBeenShown = !getCookie(cookieName);
+
+    if (alwaysShow || hasNotBeenShown) {
+      setCookie(cookieName, 'true', cookieExpirationDays);
+
+      var left, right;
+
+      if (position === 'left') {
+        left = '0';
+        right = 'auto';
+      }
+      else {
+        left = 'auto';
+        right = '0';
+      }
+
+      injectCSS('RED_ALERT_CSS',
+        '#' + domId + ' { position: fixed; right: ' + right + '; left: ' + left + '; bottom: 0px; width: 450px; height: 350px; z-index: 20000; -webkit-overflow-scrolling: touch; overflow: hidden; transition: width ' + animationDuration + 'ms ease-in, height ' + animationDuration + 'ms ease-in; } ' +
+        '#' + domId + '.RAW--maximized { width: 100%; height: 100%; } ' +
+        '#' + domId + '.RAW--closing { transform: scale(0); transform-origin: bottom right; opacity: 0; transition: transform ' + animationDuration + 'ms ease-in, opacity ' + animationDuration + 'ms ease-in; } ' +
+        '#' + domId + ' iframe { width: 100%; height: 100%; }'
+      );
+
+      createIframe();
+
+      // listen for messages from iframe
+      window.addEventListener('message', receiveMessage);
+    }
+
+    document.removeEventListener('DOMContentLoaded', init);
   }
 
   // Wait for DOM content to load.
@@ -188,9 +177,9 @@
     case 'complete':
     case 'loaded':
     case 'interactive':
-      widget.init();
+      init();
       break;
     default:
-      document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+      document.addEventListener('DOMContentLoaded', init);
   }
 })();
